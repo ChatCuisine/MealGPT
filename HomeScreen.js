71e393f804ce537e9d1f66db94829885824fbd2b
Matthew Barton <mjbarton712@gmail.com>
2023-09-24 18:22:26 -0500

    adjusting some home page stuff

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,55 +1,71 @@
-// HomeScreen.js
-
 import React from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Button, Icon } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
 import BuyCarrotsModal from './BuyCarrotsModal';
+import { useFonts } from "expo-font";
 
 const HomeScreen = () => {
     const navigation = useNavigation();
     const [isBuyCarrotsModalVisible, setIsBuyCarrotsModalVisible] = React.useState(false);
 
+    const [fontsLoaded] = useFonts({
+        BalooRegular: require("./fonts/Baloo-Regular.ttf"),
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
     return (
         <View style={{ flex: 1 }}>
             {/* Gradient Background */}
             <LinearGradient
-                colors={['#82909a', '#a05d44']}  // See how these work...might need to change.
+                colors={['#A2E1ED', '#E59758']}  // See how these work...might need to change.
                 style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
             >
                 {/* Image */}
                 <Image
                     source={require('./assets/chatcuisine_home.png')}
-                    style={{ width: '80%', height: '50%', resizeMode: 'contain' }}
+                    style={{ width: '80%', height: '50%', resizeMode: 'contain', borderRadius: '25px' }}
                 />
 
                 {/* Title and Description */}
-                <Text style={{ fontSize: 24, marginTop: 20, fontWeight: 'bold' }}>
+                <Text style={{ fontSize: 24, marginTop: 20, fontWeight: 'bold', color: 'white', fontFamily: 'BalooRegular' }}>
                     Chat Cuisine
                 </Text>
-                <Text style={{ fontSize: 12, marginTop: 10, textAlign: 'center' }}>
+                <Text style={{ width: '80%', fontSize: 14, marginTop: 10, textAlign: 'center', color: 'white', fontFamily: 'BalooRegular' }}>
                     Get some fresh and unique meal ideas based on ingredients you have. Spice up your meal life!
                 </Text>
 
                 {/* Let's Cook Button */}
                 <Button
                     title="Let's cook! 🥄"
-                    buttonStyle={{ backgroundColor: '#7bd9f1', marginTop: 20 }}
-                    titleStyle={{ color: 'white' }}
-                    containerStyle={{ borderRadius: 30, overflow: 'hidden' }}
+                    buttonStyle={{
+                        backgroundColor: '#7bd9f1',
+                        paddingVertical: 15, // Increase padding for better spacing
+                        paddingHorizontal: 30, // Increase padding for better spacing
+                        borderRadius: 20, // Adjust border radius
+                    }}
+                    titleStyle={{
+                        color: 'white',
+                        fontFamily: 'BalooRegular',
+                        fontSize: 18, // Adjust font size
+                    }}
                     raised
-                    icon={
-                        <Icon
-                            name="ios-heart"
-                            type="ionicon"
-                            color="white"
-                            size={24}
-                            iconStyle={{ marginRight: 10 }}
-                        />
-                    }
+                    icon={{
+                        name: 'ios-heart',
+                        type: 'ionicon',
+                        color: 'white',
+                        size: 24,
+                    }}
+                    containerStyle={{
+                        marginTop: 10, // Adjust marginTop
+                        borderRadius: 20, // Adjust border radius
+                        overflow: 'hidden',
+                    }}
                     onPress={() => {
-                        navigation.navigate('InputIngredients'); // Navigate to the "InputIngredients" screen
+                        navigation.navigate('InputIngredients');
                     }}
                 />
                 {/* View Previous Recipes */}
@@ -59,12 +75,12 @@ const HomeScreen = () => {
                         navigation.navigate('MyRecipes'); // Navigate to the "Recipes" screen
                     }}
                 >
-                    <Text style={{ fontSize: 12, marginRight: 5 }}>View previous recipes</Text>
+                    <Text style={{ fontSize: 14, marginRight: 5, color: 'white', fontFamily: 'BalooRegular' }}>View previous recipes</Text>
                     <Icon
                         name="chevron-right"
                         type="font-awesome"
                         size={14}
-                        color="#333"
+                        color="#FFF"
                     />
                 </TouchableOpacity>
             </LinearGradient>
@@ -76,7 +92,7 @@ const HomeScreen = () => {
                     setIsBuyCarrotsModalVisible(true); // Show the Buy Carrots modal
                 }}
             >
-                <Text style={{ fontSize: 18, fontWeight: 'bold', color: 'white' }}>10🥕</Text>
+                <Text style={{ fontSize: 18, fontWeight: 'bold', color: 'white', fontFamily: 'BalooRegular' }}>10🥕</Text>
             </TouchableOpacity>
 
             {/* Render BuyCarrotsModal */}
