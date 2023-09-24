0b648664626297a693cc27a620a4d781a3dcc307
DaltonDom <Domenighi5@gmail.com>
2023-09-24 18:45:45 -0500

    modal and design clean

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 // Import your screens
-import HomeScreen from './HomeScreen';
-import MyRecipesScreen from './MyRecipesScreen';
-import InputIngredientsScreen from './InputIngredientsScreen';
+import HomeScreen from "./HomeScreen";
+import MyRecipesScreen from "./MyRecipesScreen";
+import InputIngredientsScreen from "./InputIngredientsScreen";
 const Stack = createStackNavigator();
 
 function App() {
@@ -15,35 +15,40 @@ function App() {
     We could install expo-router if we want to try that instead - some people say it is easier?
     https://www.reddit.com/r/reactnative/comments/14apzyj/exporouter_vs_react_navigation/*/
   return (
-
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen
           name="MyRecipes"
           component={MyRecipesScreen}
           options={{
             headerStyle: {
-              backgroundColor: '#121212', // Set the background color of the header
+              backgroundColor: "#121212", // Set the background color of the header
             },
-            headerTintColor: 'white', // Set the color of the header text and buttons
+            headerTintColor: "white", // Set the color of the header text and buttons
             headerShadowVisible: false, // Set this to false to remove the header shadow
-          }} />
+          }}
+        />
         <Stack.Screen
-          name="InputIngredients"
+          name="Define Your Recipe"
           component={InputIngredientsScreen}
           options={{
             headerStyle: {
-              backgroundColor: '#121212', // Set the background color of the header
+              backgroundColor: "#121212", // Set the background color of the header
             },
-            headerTintColor: 'white', // Set the color of the header text and buttons
+            headerTintColor: "white", // Set the color of the header text and buttons
             headerShadowVisible: false, // Set this to false to remove the header shadow
-          }} />
+          }}
+        />
         {/* Adding other idea for now...
         <Stack.Screen name="GPTRecipeIdeas" component={GPTRecipeIdeasScreen} />   */}
       </Stack.Navigator>
     </NavigationContainer>
   );
-};
+}
 
 export default App;
--- HomeScreen.js
+++ HomeScreen.js
@@ -1,94 +1,145 @@
 // HomeScreen.js
 
-import React from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import { Button, Icon } from 'react-native-elements';
-import { useNavigation } from '@react-navigation/native';
-import BuyCarrotsModal from './BuyCarrotsModal';
+import React, { useRef } from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { Button, Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+import BuyCarrotsModal from "./BuyCarrotsModal";
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+} from "@gorhom/bottom-sheet";
 
 const HomeScreen = () => {
-    const navigation = useNavigation();
-    const [isBuyCarrotsModalVisible, setIsBuyCarrotsModalVisible] = React.useState(false);
+  const navigation = useNavigation();
+  const [isBuyCarrotsModalVisible, setIsBuyCarrotsModalVisible] =
+    React.useState(false);
 
-    return (
-        <View style={{ flex: 1 }}>
-            {/* Gradient Background */}
-            <LinearGradient
-                colors={['#82909a', '#a05d44']}  // See how these work...might need to change.
-                style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-            >
-                {/* Image */}
-                <Image
-                    source={require('./assets/chatcuisine_home.png')}
-                    style={{ width: '80%', height: '50%', resizeMode: 'contain' }}
-                />
+  const bottomSheetModalRef = useRef(null);
+  const snapPoints = ["85%"];
 
-                {/* Title and Description */}
-                <Text style={{ fontSize: 24, marginTop: 20, fontWeight: 'bold' }}>
-                    Chat Cuisine
-                </Text>
-                <Text style={{ fontSize: 12, marginTop: 10, textAlign: 'center' }}>
-                    Get some fresh and unique meal ideas based on ingredients you have. Spice up your meal life!
-                </Text>
+  function handlePresentModal() {
+    bottomSheetModalRef.current?.present();
+  }
 
-                {/* Let's Cook Button */}
-                <Button
-                    title="Let's cook! 🥄"
-                    buttonStyle={{ backgroundColor: '#7bd9f1', marginTop: 20 }}
-                    titleStyle={{ color: 'white' }}
-                    containerStyle={{ borderRadius: 30, overflow: 'hidden' }}
-                    raised
-                    icon={
-                        <Icon
-                            name="ios-heart"
-                            type="ionicon"
-                            color="white"
-                            size={24}
-                            iconStyle={{ marginRight: 10 }}
-                        />
-                    }
-                    onPress={() => {
-                        navigation.navigate('InputIngredients'); // Navigate to the "InputIngredients" screen
-                    }}
-                />
-                {/* View Previous Recipes */}
-                <TouchableOpacity
-                    style={{ flexDirection: 'row', alignItems: 'center', marginTop: 20 }}
-                    onPress={() => {
-                        navigation.navigate('MyRecipes'); // Navigate to the "Recipes" screen
-                    }}
-                >
-                    <Text style={{ fontSize: 12, marginRight: 5 }}>View previous recipes</Text>
-                    <Icon
-                        name="chevron-right"
-                        type="font-awesome"
-                        size={14}
-                        color="#333"
-                    />
-                </TouchableOpacity>
-            </LinearGradient>
+  return (
+    <BottomSheetModalProvider>
+      <View style={{ flex: 1 }}>
+        {/* Gradient Background */}
+        <LinearGradient
+          colors={["#82909a", "#a05d44"]} // See how these work...might need to change.
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          {/* Image */}
+          <Image
+            source={require("./assets/chatcuisine_home.png")}
+            style={{
+              width: "70%",
+              height: "50%",
+              resizeMode: "stretch",
+              borderRadius: 25,
+            }}
+          />
 
-            {/* Buy Carrots Section */}
-            <TouchableOpacity
-                style={{ flexDirection: 'row', alignItems: 'center', padding: 10, backgroundColor: '#7bd9f1' }}
-                onPress={() => {
-                    setIsBuyCarrotsModalVisible(true); // Show the Buy Carrots modal
-                }}
-            >
-                <Text style={{ fontSize: 18, fontWeight: 'bold', color: 'white' }}>10🥕</Text>
-            </TouchableOpacity>
+          {/* Title and Description */}
+          <Text
+            style={{
+              fontSize: 24,
+              marginTop: 20,
+              fontWeight: "bold",
+              color: "#faf0de",
+            }}
+          >
+            Chat Cuisine
+          </Text>
+          <Text
+            style={{
+              fontSize: 12,
+              marginTop: 10,
+              textAlign: "center",
+              width: "68%",
+              color: "#faf0de",
+            }}
+          >
+            Get some fresh and unique meal ideas based on ingredients you have.
+            Spice up your meal life!
+          </Text>
 
-            {/* Render BuyCarrotsModal */}
-            <BuyCarrotsModal
-                isVisible={isBuyCarrotsModalVisible}
-                onClose={() => {
-                    setIsBuyCarrotsModalVisible(false); // Close the Buy Carrots modal
-                }}
+          <Button
+            title="Let's cook 🍴"
+            titleStyle={{
+              fontWeight: "bold",
+            }}
+            containerStyle={{
+              borderRadius: 30,
+              overflow: "hidden",
+              width: "50%",
+              marginTop: 20,
+              color: "#faf0de",
+            }}
+            onPress={() => {
+              navigation.navigate("Define Your Recipe"); // Navigate to the "InputIngredients" screen
+            }}
+          />
+
+          <TouchableOpacity
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginTop: 20,
+            }}
+            onPress={() => {
+              navigation.navigate("MyRecipes"); // Navigate to the "Recipes" screen
+            }}
+          >
+            <Text style={{ fontSize: 12, marginRight: 5, color: "#faf0de" }}>
+              View previous recipes
+            </Text>
+            <Icon
+              name="chevron-right"
+              type="font-awesome"
+              size={10}
+              color="#faf0de"
             />
-        </View>
-    );
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: 35,
+              width: "32%",
+              height: "11%",
+            }}
+            onPress={handlePresentModal}
+          >
+            <Text
+              style={{
+                fontSize: 18,
+                fontWeight: "bold",
+                color: "white",
+              }}
+            >
+              10 🥕
+            </Text>
+          </TouchableOpacity>
+        </LinearGradient>
+
+        {/* Render BuyCarrotsModal */}
+        <BottomSheetModal
+          ref={bottomSheetModalRef}
+          index={0}
+          snapPoints={snapPoints}
+        >
+          <View>
+            <Text>Hello</Text>
+          </View>
+        </BottomSheetModal>
+      </View>
+    </BottomSheetModalProvider>
+  );
 };
 
 export default HomeScreen;
-
