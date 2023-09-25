2d251e41fcfbfbe4d1636a29d4beff43f224ec38
DaltonDom <Domenighi5@gmail.com>
2023-09-24 22:12:10 -0500

    added slider and added some other checks

--- InputIngredientsScreen.js
+++ InputIngredientsScreen.js
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity, Switch } from "react-native";
-import { Slider } from "@react-native-community/slider";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Switch,
+  Dimensions,
+  StyleSheet,
+  Button,
+} from "react-native";
+import Slider from "@react-native-community/slider";
 
 /* This is the screen for gathering data (ingredients, etc.) that will be input for the chatGPT prompt */
 const InputIngredientsScreen = ({ navigation }) => {
@@ -8,6 +17,11 @@ const InputIngredientsScreen = ({ navigation }) => {
   const [mealPrepTime, setMealPrepTime] = useState(15);
   const [dietaryPreferences, setDietaryPreferences] = useState({});
   const [includeExtraIngredients, setIncludeExtraIngredients] = useState(false);
+  const screenWidth = Dimensions.get("window").width;
+
+  const onSliderValueChange = (value) => {
+    setMealPrepTime(value);
+  };
 
   const handleGenerateRecipes = () => {
     // Implement logic to generate recipes based on user input here
@@ -48,22 +62,48 @@ const InputIngredientsScreen = ({ navigation }) => {
         value={{ key: "ingredients" }}
         onChangeText={(text) => setIngredients(text)}
       />
+      <Text style={{ color: "white", fontSize: 16 }}>
+        What condiments, seasonings, and sauces you have on hand?
+      </Text>
+
+      <Text style={{ color: "white", fontSize: 16 }}>What type of meal?</Text>
+      <View style={styles.container}>
+        <Button
+          title="Breakfast"
+          onPress={() => {}}
+          buttonStyle={{
+            backgroundColor: "#7bd9f1",
+            paddingVertical: 15,
+            paddingHorizontal: 30,
+            borderRadius: 20,
+          }}
+          containerStyle={{
+            borderRadius: 30,
+            overflow: "hidden",
+            width: "50%",
+            marginTop: 20,
+            color: "#faf0de",
+            marginTop: 10,
+            borderRadius: 20,
+            overflow: "hidden",
+          }}
+        />
+      </View>
 
       {/* Meal Prep Time Slider */}
       <Text style={{ color: "white", fontSize: 16, marginTop: 16 }}>
-        Preferred Meal Prep Time: ({mealPrepTime} minutes)
+        Preferred Meal Prep Time: {mealPrepTime} minutes
       </Text>
-      {/*<Slider
-                minimumValue={15}
-                maximumValue={60}
-                step={5}
-                value={mealPrepTime}
-                onValueChange={(value) => setMealPrepTime(value)}
-                minimumTrackTintColor="#7bd9f1" // Customize the color of the slider track
-                maximumTrackTintColor="white" // Customize the color of the slider track beyond the thumb
-                thumbTintColor="#7bd9f1" // Customize the color of the thumb (slider handle)
-            />*/}
-
+      <Slider
+        style={{ width: "100%", height: 40 }}
+        minimumValue={15}
+        maximumValue={60}
+        step={5}
+        minimumTrackTintColor="#FFFFFF"
+        maximumTrackTintColor="#000000"
+        value={mealPrepTime}
+        onValueChange={onSliderValueChange}
+      />
       {/* Dietary Preferences */}
       <Text style={{ color: "white", fontSize: 16, marginTop: 16 }}>
         Dietary Preferences
@@ -96,7 +136,7 @@ const InputIngredientsScreen = ({ navigation }) => {
           I have not listed
         </Text>
       </View>
-
+      <Text style={{ color: "white", fontSize: 16 }}>Level of difficulty</Text>
       {/* Generate Recipes Button */}
       <TouchableOpacity
         style={{
@@ -113,5 +153,13 @@ const InputIngredientsScreen = ({ navigation }) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+});
 
 export default InputIngredientsScreen;
