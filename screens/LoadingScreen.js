402fc83de29fd7a1bb2c3b61b849b2988c013da0
DaltonDom <Domenighi5@gmail.com>
2023-10-02 19:49:19 -0500

    add firebase logic to store recipes
    npm install firebase

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -1,72 +1,108 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { ActivityIndicator, Animated, Easing, Text } from 'react-native';
-import styled from 'styled-components';
-import axios from 'axios';
-import { useNavigation } from '@react-navigation/native';
-import { OPENAI_API_KEY } from '@env';
-import { createChatCompletion } from '../api/ChatGPTService';
+import React, { useState, useEffect, useRef } from "react";
+import { ActivityIndicator, Animated, Easing, Text } from "react-native";
+import styled from "styled-components";
+import axios from "axios";
+import { useNavigation } from "@react-navigation/native";
+import { OPENAI_API_KEY } from "@env";
+import { createChatCompletion } from "../api/ChatGPTService";
+import { collection, addDoc } from "firebase/firestore"; // Import Firestore functions
+import { db } from "../firebase/firebase-config"; // Import your Firebase configuration
 
 const LoadingScreen = ({ route }) => {
-    // const navigation = useNavigation();
-    const [isLoading, setIsLoading] = useState(true);
-    const [response, setResponse] = useState('');
-    const [error, setError] = useState(null);
-    const fadeAnim = useRef(new Animated.Value(0)).current;
-    const apiKey = OPENAI_API_KEY;
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const userPrompt = route.params.userPrompt;
-                const axiosInstance = axios.create({
-                    baseURL: 'https://api.openai.com/v1/completions',
-                    headers: {
-                        'Authorization': `Bearer ${apiKey}`,
-                        'Content-Type': 'application/json',
-                    },
-                });
+  // const navigation = useNavigation();
+  const [isLoading, setIsLoading] = useState(true);
+  const [response, setResponse] = useState("");
+  const [error, setError] = useState(null);
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const apiKey = OPENAI_API_KEY;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userPrompt = route.params.userPrompt;
+        const axiosInstance = axios.create({
+          baseURL: "https://api.openai.com/v1/completions",
+          headers: {
+            Authorization: `Bearer ${apiKey}`,
+            "Content-Type": "application/json",
+          },
+        });
 
-                const chatResponse = await createChatCompletion(userPrompt, apiKey, axiosInstance);
-                setResponse(chatResponse);
-                setIsLoading(false);
+        const chatResponse = await createChatCompletion(
+          userPrompt,
+          apiKey,
+          axiosInstance
+        );
+        setResponse(chatResponse);
+        setIsLoading(false);
 
-                // Fade in the response text
-                Animated.timing(fadeAnim, {
-                    toValue: 1,
-                    duration: 500,
-                    easing: Easing.linear,
-                    useNativeDriver: true,
-                }).start();
-            } catch (err) {
-                setError(err.message || 'An error occurred');
-                setIsLoading(false);
-            }
-        };
+        // Fade in the response text
+        Animated.timing(fadeAnim, {
+          toValue: 1,
+          duration: 500,
+          easing: Easing.linear,
+          useNativeDriver: true,
+        }).start();
+      } catch (err) {
+        setError(err.message || "An error occurred");
+        setIsLoading(false);
+      }
+    };
 
-        fetchData();
-    }, [route.params.userPrompt]);
+    fetchData();
+  }, [route.params.userPrompt]);
 
-    return (
-        <ScrollContainer>
-            <LoadingImage source={require('../assets/chatcuisine_loadingrecipes.png')} />
-            {isLoading ? (
-                <LoadingView>
-                    <ActivityIndicator size="large" color="white" style={{ marginTop: 16 }} />
-                    <LoadingText>Our rabbits are dreaming up your cuisine... this may take up to 30 seconds.</LoadingText>
-                </LoadingView>
-            ) : (
-                <Animated.View style={{ opacity: fadeAnim }}>
-                    <ResponseText>{response}</ResponseText>
-                </Animated.View>
-            )}
-            {!isLoading && error && <ErrorText>{error}</ErrorText>}
-        </ScrollContainer>
-    );
+  return (
+    <ScrollContainer>
+      <LoadingImage
+        source={require("../assets/chatcuisine_loadingrecipes.png")}
+      />
+      {isLoading ? (
+        <LoadingView>
+          <ActivityIndicator
+            size="large"
+            color="white"
+            style={{ marginTop: 16 }}
+          />
+          <LoadingText>
+            Our rabbits are dreaming up your cuisine... this may take up to 30
+            seconds.
+          </LoadingText>
+        </LoadingView>
+      ) : (
+        <Animated.View style={{ opacity: fadeAnim }}>
+          <ResponseText>{response}</ResponseText>
+        </Animated.View>
+      )}
+      {!isLoading && error && <ErrorText>{error}</ErrorText>}
+      <StoreRecipes response={response} />
+    </ScrollContainer>
+  );
 };
 
 export default LoadingScreen;
 
+const StoreRecipes = ({ response }) => {
+  useEffect(() => {
+    const addResponseToFirestore = async () => {
+      try {
+        const docRef = await addDoc(collection(db, "recipes"), {
+          response: response, // Add the response to Firestore
+        });
+        console.log("Recipe written with ID: ", docRef.id);
+      } catch (error) {
+        console.error("Error adding recipe: ", error);
+      }
+    };
+    // Call the function when the component loads (ensure response is not null)
+    if (response) {
+      addResponseToFirestore();
+    }
+  }, [response]);
+  return null;
+};
+
 const ScrollContainer = styled.ScrollView`
-  flexGrow: 1;
+  flexgrow: 1;
   background-color: #121212;
   padding: 16px;
 `;
